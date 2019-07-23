192bff7f5a9d5e5093d763292c44bc569112ef72
NikolaL87 <dzoni.lovic@gmmail.com>
2019-07-23 20:52:08 +0200

    Get view controller added. Mapbox for single tour added. Get login form controller added. Login, Overview, Tour - Pug templates added.

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,8 +13,22 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
+  });
+
+  // 2) Build template
+  // 3) Render that template using tour data from 1
   res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
+    title: `${tour.name} Tour`,
+    tour
+  });
+});
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account'
   });
 };
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,7 @@ const views = require('../controllers/viewsController');
 const router = express.Router();
 
 router.get('/', views.getOverview);
-router.get('/tour', views.getTour);
+router.get('/tour/:slug', views.getTour);
+router.get('/login', views.getLoginForm);
 
 module.exports = router;
