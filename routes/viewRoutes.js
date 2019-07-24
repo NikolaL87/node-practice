3e46d04d1a3fa25b1536052f332b54838f81c330
NikolaL87 <dzoni.lovic@gmmail.com>
2019-07-24 20:14:06 +0200

    Get account controller added. Update user data controller added. Update user data added. Reset password functionality added. Update settings module added. Submit user data route added. Update account and login pug templates with data.

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const views = require('../controllers/viewsController');
+const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.use(authController.isLoggedIn);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
+router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
+router.get('/me', authController.protect, viewsController.getAccount);
 
-router.get('/', views.getOverview);
-router.get('/tour/:slug', views.getTour);
-router.get('/login', views.getLoginForm);
+router.post(
+  '/submit-user-data',
+  authController.protect,
+  viewsController.updateUserData
+);
 
 module.exports = router;
